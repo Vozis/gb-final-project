360085fe6a6c0fd79c9b9ba1afc5209f62bd7baf
Tih0n <tihon_06@mail.ru>
2023-05-24 21:49:58 +0300

    добавил изменение решения ошибки с нодмодулями

--- project/libs/auth-main/src/lib/form/form.tsx
+++ project/libs/auth-main/src/lib/form/form.tsx
@@ -14,6 +14,7 @@ export function Form(props: FormProps) {
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const onSubmit = async (data: any) => {
     try {
       // Меняем здесь
