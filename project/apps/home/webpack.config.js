e53cb95af28c7b6697aaf657cd98f1196800b246
Ilya <sizov.ilya1996@gmail.com>
2023-05-24 21:15:14 +0300

    correct mistake with not found modules

--- project/apps/home/webpack.config.js
+++ project/apps/home/webpack.config.js
@@ -1,10 +1,14 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
-const { withModuleFederation } = require('@nx/react/module-federation');
+const withModuleFederation = require('@nx/react/module-federation');
+const { merge } = require('webpack-merge');
+
+const moduleFederationConfig = require('./module-federation.config');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 const baseConfig = require('./module-federation.config');
 
-const config = {
+const federationModuleConfig = {
   ...baseConfig,
 };
 
@@ -12,5 +16,13 @@ const config = {
 module.exports = composePlugins(
   withNx(),
   withReact(),
-  withModuleFederation(config),
+  async (config, { options, context }) => {
+    config.plugins.push(new NodePolyfillPlugin());
+
+    const federatedModules = await withModuleFederation({
+      ...federationModuleConfig,
+    });
+
+    return merge(federatedModules(config, { options, context }), {});
+  },
 );
--- project/libs/shared/utils/src/index.ts
+++ project/libs/shared/utils/src/index.ts
@@ -1,2 +1,1 @@
 export * from './lib/errorCatch';
-export * from './lib/file-upload.helper';
