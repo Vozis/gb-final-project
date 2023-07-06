d20b032483f6d88b6a913059540fe1dbca1ed59e
Tih0n <tihon_06@mail.ru>
2023-07-06 22:44:14 +0300

    сделал Notifications Events Friends Comments

--- project/libs/notifications-main/src/lib/notifications-comments/notifications-comments.tsx
+++ project/libs/notifications-main/src/lib/notifications-comments/notifications-comments.tsx
@@ -1,12 +1,67 @@
+import { INotification } from '@project/shared/types';
+import { Link } from 'react-router-dom';
+import Avatar from '../../../../shared/ui/src/lib/avatar/avatar';
 import styles from './notifications-comments.module.scss';
-
 /* eslint-disable-next-line */
-export interface NotificationsCommentsProps {}
+export interface NotificationsCommentsProps {
+  data: INotification;
+}
 
-export function NotificationsComments(props: NotificationsCommentsProps) {
+export function NotificationsComments(data: NotificationsCommentsProps) {
+  console.log('data: ', data);
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to NotificationsComments!</h1>
+    // <div className={styles['container']}>
+    //   <h1>Welcome to NotificationsComments!</h1>
+    // </div>
+    <div className={styles.notifications}>
+      <div className={styles.notifications__list}>
+        <div key={data.data.id}>
+          {data.data.type === 'COMMENT_CREATE' ? (
+            <div className={styles.event_container}>
+              {/* <img src={data.data.user.avatarPath} alt={'avatar'} /> */}
+
+              <Avatar imagePath={data.data.user.avatarPath} />
+              <p>
+                Ваш друг &nbsp;
+                <Link
+                  to={`/users/${data.data.user.id}`}
+                  className={styles.event_span}
+                >
+                  {data.data.user.firstName} {data.data.user.lastName}
+                </Link>
+                <span>
+                  {' '}
+                  добавил коментарий "{data.data.sourceData}" к событию&nbsp;
+                </span>
+                <Link
+                  to={`/events/${data.data.sourceId}`}
+                  className={styles.event_span}
+                >
+                  {data.data.additionalData}
+                </Link>
+              </p>
+            </div>
+          ) : (
+            // EVENT_COMPLETE
+            <div className={styles.event_container}>
+              <Avatar imagePath={data.data.user.avatarPath} />
+              <p>
+                Ваш друг &nbsp;
+                <Link
+                  to={`/users/${data.data.user.id}`}
+                  className={styles.event_span}
+                >
+                  {data.data.user.firstName} {data.data.user.lastName}
+                </Link>
+                &nbsp;
+                <span>
+                  ответил на вашь коментарий "{data.data.additionalData}"&nbsp;
+                </span>
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
