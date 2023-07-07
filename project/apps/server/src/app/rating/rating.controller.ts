ec58efdfde1eb2409f1fc172bafa817ca61d7414
Ilya <sizov.ilya1996@gmail.com>
2023-07-08 01:44:11 +0300

    start fixing rating

--- project/apps/server/src/app/rating/rating.controller.ts
+++ project/apps/server/src/app/rating/rating.controller.ts
@@ -1,17 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { RatingService } from './rating.service';
 import { CreateRatingDto } from './dto/create-rating.dto';
-import { UpdateRatingDto } from './dto/update-rating.dto';
-import {Auth} from "../auth/decorators/auth.decorator";
-import {User} from "../auth/decorators/user.decorator";
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @Controller('rating')
 export class RatingController {
